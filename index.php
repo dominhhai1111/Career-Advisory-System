547be5b1433c2a7f75cd467a53fc0a22956f91cc
Do Minh Hai <hai.do.intern@ntq-solution.com.vn>
2017-11-23 13:39:30 +0700

    create view

--- index.php
+++ index.php
@@ -11,5 +11,32 @@
 </head>
 <body>
 	<h2>Hệ thống tư vấn chọn ngành</h2>
+    <div class="container">
+        <div class="introduce text-center">
+            <p>* Điền đầy đủ thông tin sau đây, hệ thống sẽ tư vấn cho bạn ngành nghề phù hợp với bạn!</p>
+        </div>
+        <div class="content">
+            <form action="./execution.php" method="post">
+                <div class="info">
+                    <p>Sở thích cá nhân:<p>
+                    <div class="row">
+                        <p class="col-sm-3"><input type="checkbox" name="favorite" value="item 1"> Item 1</p>
+                        <p class="col-sm-3"><input type="checkbox" name="favorite" value="item 2"> Item 2</p>
+                        <p class="col-sm-3"><input type="checkbox" name="favorite" value="item 1"> Item 1</p>
+                        <p class="col-sm-3"><input type="checkbox" name="favorite" value="item 2"> Item 2</p>
+                        <p class="col-sm-3"><input type="checkbox" name="favorite" value="item 1"> Item 1</p>
+                        <p class="col-sm-3"><input type="checkbox" name="favorite" value="item 2"> Item 2</p>
+                    </div>
+                </div>
+                <div class="info">
+                    <p>Năng khiếu:</p>
+                </div>
+                <div class="info">
+                    <p>Điểm thi thử các môn:</p>
+                </div>
+                <input type="submit" name="submit" value="Gửi" class="center-block">
+            </form>
+        </div>
+    </div>
 </body>
 </html>
